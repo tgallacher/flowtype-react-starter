4bc59054d7fde95660020b59d615611c8e4ae151
Leslie Hoare <iam@lesleh.co.uk>
2018-07-20 16:11:38 +0100

    Add typescript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx']
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
       }
     ]
   }
